bde5cd94fe07c5fd7f668cbba17dc598466b12a2
inosy22 <inos.sj@gmail.com>
2019-12-05 05:24:47 +0900

    :recycle: Divide calculator component

---
+++ src/components/speed_checker/CalculatorCard.vue
@@ -0,0 +1,168 @@
+<template>
+  <v-card>
+    <v-card-title>{{ props.title }}</v-card-title>
+    <v-card-text>
+      <div>
+        <v-combobox
+          v-model="state.pokemonName"
+          :items="pokemonsForSearch"
+          label="ポケモン"
+          dense
+        />
+      </div>
+      <div>
+        <v-radio-group v-model="state.natureCorrection" label="性格補正" row>
+          <v-radio label="上昇" value="1" color="white" />
+          <v-radio label="なし" value="0" color="white" />
+          <v-radio label="下降" value="-1" color="white" />
+        </v-radio-group>
+      </div>
+      <div>
+        <v-combobox
+          v-model="state.effortValue"
+          :items="effortValueInputs"
+          label="努力値"
+          dense
+        />
+      </div>
+      <div>
+        <v-select
+          v-model="state.rank"
+          :items="rankItems"
+          label="補正ランク"
+          dense
+        />
+      </div>
+      <div>
+        <v-checkbox
+          v-model="state.hasScarf"
+          label="こだわりスカーフ"
+          color="white"
+        />
+        <v-checkbox
+          v-model="state.isParalysis"
+          label="まひ状態"
+          color="white"
+        />
+        <v-checkbox
+          v-model="state.isActiveTailwind"
+          label="おいかぜ"
+          color="white"
+        />
+        <v-checkbox
+          v-model="state.isActiveWeather"
+          label="天候の特性での補正"
+          color="white"
+        />
+      </div>
+    </v-card-text>
+    <v-card-subtitle>
+      <h3>素早さ実数値: {{ compute.speed.value }}</h3>
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+import { createComponent, computed, reactive } from '@vue/composition-api'
+import BaseStatsCalculator from '~/lib/pokemon/BaseStatsCalculator'
+import SpeedStatsCalculator from '~/lib/pokemon/SpeedStatsCalculator'
+
+/**
+ * カタカナからひらがなに変換
+ * (IncrementalSearch用)
+ *
+ * @param str 対象のカタカナの文字列
+ */
+// function kanaToHira(str) {
+//   return str.replace(/[\u30A1-\u30F6]/g, function(match) {
+//     const chr = match.charCodeAt(0) - 0x60
+//     return String.fromCharCode(chr)
+//   })
+// }
+
+/**
+ * VueComponent
+ */
+export default createComponent({
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  setup(props, ctx) {
+    // load json
+    const pokemons = require('~/assets/data/pokemon.json')
+
+    // reactive properties
+    const state = reactive({
+      pokemonName: '',
+      natureCorrection: '1',
+      level: '50',
+      effortValue: '252',
+      rank: '0',
+      hasScarf: false,
+      isParalysis: false,
+      isActiveTailwind: false,
+      isActiveWeather: false
+    })
+
+    // computed properties
+    const compute = {
+      speed: computed(() => {
+        // ポケモンが存在しなければ計算不能
+        if (pokemons[state.pokemonName] === undefined) {
+          return '???'
+        }
+        const speedStatsCalculator = new SpeedStatsCalculator()
+        speedStatsCalculator.setBaseStats(pokemons[state.pokemonName].s)
+        speedStatsCalculator.setNatureCorrection(state.natureCorrection)
+        speedStatsCalculator.setEffortValue(state.effortValue)
+        speedStatsCalculator.setLevel(state.level)
+        speedStatsCalculator.setRank(state.rank)
+        speedStatsCalculator.setHasScarf(state.hasScarf)
+        speedStatsCalculator.setIsParalysis(state.isParalysis)
+        speedStatsCalculator.setIsActiveTailwind(state.isActiveTailwind)
+        speedStatsCalculator.setIsActiveWeather(state.isActiveWeather)
+        return speedStatsCalculator.calc()
+      })
+    }
+
+    // create incremental search pokemon items
+    const pokemonsForSearch = Object.keys(pokemons)
+
+    // create incremental search ev items
+    const effortValueInputs = [BaseStatsCalculator.MaxEffortValue]
+    for (
+      let ev = BaseStatsCalculator.MinEffortValue;
+      ev < BaseStatsCalculator.MaxEffortValue;
+      ev += 4
+    ) {
+      effortValueInputs.push(String(ev))
+    }
+
+    // create rank items
+    const rankItems = []
+    for (
+      let rank = BaseStatsCalculator.MaxRank;
+      rank >= BaseStatsCalculator.MinRank;
+      rank--
+    ) {
+      let rankStr = String(rank)
+      if (rank > 0) {
+        rankStr = '+' + rankStr
+      }
+      rankItems.push(rankStr)
+    }
+
+    return {
+      props,
+      state,
+      compute,
+      pokemonsForSearch,
+      effortValueInputs,
+      rankItems
+    }
+  }
+})
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,180 +1,39 @@
 <template>
   <v-layout row wrap align-center>
     <v-flex xs12>
-      <h2>ポケモン素早さチェッカー for 剣盾</h2>
+      <h2>ポケモン素早さチェッカーfor剣盾</h2>
     </v-flex>
-    <v-flex xs12 sm6>
-      <v-card>
-        <v-card-title>自分のポケモン</v-card-title>
-        <v-card-text>
-          <div>
-            <v-combobox
-              v-model="state.myPokemonName"
-              :items="pokemonsForSearch"
-              label="ポケモン"
-              dense
-            />
-          </div>
-          <div>
-            <v-radio-group
-              v-model="state.myNatureCorrection"
-              label="性格補正"
-              row
-            >
-              <v-radio label="上昇" value="1" color="white" />
-              <v-radio label="なし" value="0" color="white" />
-              <v-radio label="下降" value="-1" color="white" />
-            </v-radio-group>
-          </div>
-          <div>
-            <v-combobox
-              v-model="state.myEffortValue"
-              :items="effortValueInputs"
-              label="努力値"
-              dense
-            />
-          </div>
-          <div>
-            <v-select
-              v-model="state.myRank"
-              :items="rankItems"
-              label="補正ランク"
-              dense
-            />
-          </div>
-          <div>
-            <v-checkbox
-              v-model="state.myHasScarf"
-              label="こだわりスカーフ"
-              color="white"
-            />
-            <v-checkbox
-              v-model="state.myIsParalysis"
-              label="まひ状態"
-              color="white"
-            />
-            <v-checkbox
-              v-model="state.myIsActiveTailwind"
-              label="おいかぜ"
-              color="white"
-            />
-            <v-checkbox
-              v-model="state.myIsActiveWeather"
-              label="天候の特性での補正"
-              color="white"
-            />
-          </div>
-        </v-card-text>
-        <v-card-subtitle>
-          <h3>素早さ実数値: {{ compute.mySpeed.value }}</h3>
-        </v-card-subtitle>
-      </v-card>
+    <v-flex xs12 sm6 class="card-container">
+      <CalculatorCard :title="`自分のポケモン`" />
     </v-flex>
-    <v-flex xs12 sm6>
+    <v-flex xs12 sm6 class="card-container">
+      <CalculatorCard :title="`相手のポケモン`" />
+    </v-flex>
+    <v-flex xs12 class="card-container">
       <v-card>
-        <v-card-title>相手のポケモン</v-card-title>
-        <v-card-text>
-          <v-combobox :items="pokemonsForSearch" />
-        </v-card-text>
+        <v-card-title>
+          子コンポーネントから計算結果を `emit`
+          で受け取って、ここに比較結果を入れれば終わり！
+        </v-card-title>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 
+<style scoped>
+.card-container {
+  padding: 5px;
+}
+</style>
+
 <script>
-import { createComponent, computed, reactive } from '@vue/composition-api'
-import BaseStatsCalculator from '~/lib/pokemon/BaseStatsCalculator'
-import SpeedStatsCalculator from '~/lib/pokemon/SpeedStatsCalculator'
+import { createComponent } from '@vue/composition-api'
+import CalculatorCard from '~/components/speed_checker/CalculatorCard'
 
-/**
- * カタカナからひらがなに変換
- * (IncrementalSearch用)
- *
- * @param str 対象のカタカナの文字列
- */
-// function kanaToHira(str) {
-//   return str.replace(/[\u30A1-\u30F6]/g, function(match) {
-//     const chr = match.charCodeAt(0) - 0x60
-//     return String.fromCharCode(chr)
-//   })
-// }
-
-/**
- * VueComponent
- */
 export default createComponent({
-  setup() {
-    // load json
-    const pokemons = require('~/assets/data/pokemon.json')
-
-    // reactive properties
-    const state = reactive({
-      myPokemonName: '',
-      myNatureCorrection: '1',
-      myLevel: '50',
-      myEffortValue: '252',
-      myRank: '0',
-      myHasScarf: false,
-      myIsParalysis: false,
-      myIsActiveTailwind: false,
-      myIsActiveWeather: false
-    })
-
-    // computed properties
-    const compute = {
-      mySpeed: computed(() => {
-        // ポケモンが存在しなければ計算不能
-        if (pokemons[state.myPokemonName] === undefined) {
-          return '???'
-        }
-        const speedStatsCalculator = new SpeedStatsCalculator()
-        speedStatsCalculator.setBaseStats(pokemons[state.myPokemonName].s)
-        speedStatsCalculator.setNatureCorrection(state.myNatureCorrection)
-        speedStatsCalculator.setEffortValue(state.myEffortValue)
-        speedStatsCalculator.setLevel(state.myLevel)
-        speedStatsCalculator.setRank(state.myRank)
-        speedStatsCalculator.setHasScarf(state.myHasScarf)
-        speedStatsCalculator.setIsParalysis(state.myIsParalysis)
-        speedStatsCalculator.setIsActiveTailwind(state.myIsActiveTailwind)
-        speedStatsCalculator.setIsActiveWeather(state.myIsActiveWeather)
-        return speedStatsCalculator.calc()
-      })
-    }
-
-    // create incremental search pokemon items
-    const pokemonsForSearch = Object.keys(pokemons)
-
-    // create incremental search ev items
-    const effortValueInputs = [BaseStatsCalculator.MaxEffortValue]
-    for (
-      let ev = BaseStatsCalculator.MinEffortValue;
-      ev < BaseStatsCalculator.MaxEffortValue;
-      ev += 4
-    ) {
-      effortValueInputs.push(String(ev))
-    }
-
-    // create rank items
-    const rankItems = []
-    for (
-      let rank = BaseStatsCalculator.MaxRank;
-      rank >= BaseStatsCalculator.MinRank;
-      rank--
-    ) {
-      let rankStr = String(rank)
-      if (rank > 0) {
-        rankStr = '+' + rankStr
-      }
-      rankItems.push(rankStr)
-    }
-
-    return {
-      state,
-      compute,
-      pokemonsForSearch,
-      effortValueInputs,
-      rankItems
-    }
-  }
+  components: {
+    CalculatorCard
+  },
+  setup() {}
 })
 </script>
