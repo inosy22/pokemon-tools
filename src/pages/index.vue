c6abd29d5de9aa1dc4ee757ee1eacd474eb87795
inosy22 <inos.sj@gmail.com>
2019-12-09 04:13:11 +0900

    :recycle: fix OGP

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <a href="./speed-checker">ポケモン素早さ比較 for 剣盾</a>
+  <ul>
+    <li>
+      <h2>
+        <a href="./speed-checker">ポケモン素早さ比較 for 剣盾</a>
+      </h2>
+    </li>
+  </ul>
 </template>
