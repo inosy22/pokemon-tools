6a29c4b9a8c6b9e62ef546d2c6d8c991441c7178
inosy22 <inos.sj@gmail.com>
2019-12-09 03:56:07 +0900

    :sparkles: Add OGP etc...

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,18 +7,22 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s - ' + 'ポケモンツール',
+    title: 'ポケモン素早さ比較 for 剣盾',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+      { hid: 'description', name: 'description', content: 'すぐに結果がわかるポケモン剣盾用素早さ比較ツールです！by @inosy22' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@inosy22' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:title', property: 'og:title', content: 'ポケモン素早さ比較 for 剣盾' },
+      { hid: 'og:url', property: 'og:url', content: 'https://pokemon-tools.netlify.com' },
+      { hid: 'og:description', property: 'og:description', content: 'ポケモン素早さ比較 for 剣盾' },
+      { hid: 'og:image', property: 'og:image', content: '/speed-ball.png' },
+      { hid: 'og:site_name', name: 'og:site_name', content: 'ポケモン素早さ比較 for 剣盾' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/speed-ball.ico' }]
   },
   /*
    ** Customize the progress-bar color
